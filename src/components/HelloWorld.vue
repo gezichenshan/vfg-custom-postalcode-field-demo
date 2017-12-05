89ebfe000192a6b9050c378b0feb7ad42d7c75b3
Jiadong.Yu <jiadong@pandu.ie>
2017-12-05 11:15:56 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="panel-body">
+    <vue-form-generator :schema="schema" :model="model" :options="formOptions"></vue-form-generator>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueFormGenerator from "vue-form-generator";
+import fieldPostcode from "./fieldPostcode.vue";
+
+Vue.use(VueFormGenerator);
+Vue.component("fieldPostcode", fieldPostcode);
+
+export default {
+  data(){
+    return{
+      model: {
+        id: 1,
+        name: "John Doe",
+        password: "J0hnD03!x4",
+        skills: ["Javascript", "VueJS"],
+        email: "john.doe@gmail.com",
+        status: true
+      },
+
+      schema: {
+        fields: [{
+          type: "postcode",
+          label: "Código postal de la empresa",
+          model: "postalCode",
+          maxlength: 5,
+          required: true,
+          validator: function (value, field, model) {
+            console.log('tests')
+          },
+          styleClasses: 'col-12 col-sm-6 col-md-4',
+        }]
+      },
+
+      formOptions: {
+        validateAfterLoad: true,
+        validateAfterChanged: true,
+        validateDebounceTime: 500
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/fieldPostcode.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+      class="form-control"
+      type="tel"
+      v-model="value"
+      :disabled="disabled"
+      :maxlength="schema.maxlength"
+      :placeholder="schema.placeholder"
+      :readonly="schema.readonly"
+      @input="onEnter($event)"
+      >
+</template>
+
+<script>
+  import { abstractField } from "vue-form-generator";
+  export default {
+    mixins: [ abstractField ],
+    methods: {
+      onEnter(event){
+        this.value = this.value.replace(/[^0-9-]+/,'')
+      }
+    }
+  };
+</script>
